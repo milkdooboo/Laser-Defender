0dd79d6b9bebf4edff36af00d57289c2c2dc984d
milkdooboo <kangsuna.korea@gmail.com>
2024-03-11 19:45:59 +0900

    Laser Defender

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfigSO> waveConfigs;
+    [SerializeField] float timeBetweenWaves = 0f;
+    [SerializeField] bool isLooping;
+    WaveConfigSO currentWave;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyWaves());
+    }
+
+    public WaveConfigSO GetCurrentWave()
+    {
+        return currentWave;
+    }
+
+    IEnumerator SpawnEnemyWaves() // IEnumerator: 셀 수 있는 무언가를 돌려주는 메소드
+    {
+        do
+        {
+            foreach (WaveConfigSO wave in waveConfigs)
+            {
+                currentWave = wave;
+                for(int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    Instantiate(currentWave.GetEnemyPrefab(i),
+                                currentWave.GetStartingWayPoint().position,
+                                Quaternion.Euler(0,0,180),
+                                transform);
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
+            }
+        }
+        while(isLooping);
+    }
+}
